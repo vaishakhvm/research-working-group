e874b015983467dcea7ae4f2359db8960d10eaab
krypc-sharath <sharathmohan@krypc.com>
2021-06-23 10:48:56 +0530

    fixed test code

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -2,8 +2,10 @@ package hello
 
 import "testing"
 
+
+
 func TestGreetsGitHub(t *testing.T) {
-	result := Greet()
+	result := hello.Greet()
 	if result != "Hello GitHub Actions. Dev.to is awesome" {
 		t.Errorf("Greet() = %s; want Hello GitHub Actions. Dev.to is awesome", result)
 	}
