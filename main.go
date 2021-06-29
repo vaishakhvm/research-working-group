8dfcf6c755e1c642c3c1351ccf976ca774337e2b
krypc-sharath <84719334+krypc-sharath@users.noreply.github.com>
2021-06-29 16:57:03 +0530

    Update main.go
    Added a test function to be tested via workflow.

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "testing"
+)
+
+func IntMin(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+func TestIntMinBasic(t *testing.T) {
+    ans := IntMin(2, -2)
+    if ans != -2 {
+
+        t.Errorf("IntMin(2, -2) = %d; want -2", ans)
+    }
+}
 
 func helloworld() string {
 	return "Hello   World!!"
 }
 
 func main() {
-  fmt.Println(helloworld())
+	fmt.Println(helloworld())
+	fmt.Println("This a lucky number to be ignored:",IntMin(10,20))
 }
