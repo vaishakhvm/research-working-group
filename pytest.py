c2b099069b5eeab18c51b7f41073d3ae72a47e51
krypc-sharath <84719334+krypc-sharath@users.noreply.github.com>
2021-07-26 19:02:56 +0530

    Update pytest.py
    "Print saved credentials"

--- pytest.py
+++ pytest.py
@@ -1,6 +1,12 @@
 import sys
+from dotenv import load_dotenv
 
 version_tag = str(sys.argv[1])
 
 with open(".env", "w") as f:
     f.write("version={}".format(version_tag))
+    
+load_dotenv()
+
+tag_version = os.getenv('version')
+print(tag_version+" Passed!!")
