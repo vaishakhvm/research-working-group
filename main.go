b049214fedd70fc75be129ba8e2e67b1e6786495
VaishakhKrypC <vaishakh@krypc.com>
2021-08-02 13:16:24 +0530

    main file changes

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 )
 
 func IntMin(a, b int) int {
-    if a < b {
-        return a
-    }
-    return b
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func TestIntMinBasic(t *testing.T) {
-    ans := IntMin(2, -2)
-    if ans != -2 {
+	ans := IntMin(2, -2)
+	if ans != -2 {
 
-        t.Errorf("IntMin(2, -2) = %d; want -2", ans)
-    }
+		t.Errorf("IntMin(2, -2) = %d; want -2", ans)
+	}
 }
 
 func helloworld() string {
-	return "Hello World!!"
+	return "Hello World"
 }
 
 func main() {
 	fmt.Println(helloworld())
-	fmt.Println("This a lucky number to be ignored:",IntMin(10,20))
+	fmt.Println("This a lucky number to be ignored:", IntMin(10, 20))
 }
